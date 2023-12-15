c1a34434ff36b286a69cff922206428ce749e3ec
Luca Capra <luca.capra@spindox.it>
2023-12-15 08:13:35 +0100

    keep config

--- src/query.py
+++ src/query.py
@@ -8,10 +8,10 @@ from langchain.vectorstores.base import VectorStoreRetriever
 
 model_name = os.environ.get('MODEL_NAME', 'mistral')
 model_name_emb = os.environ.get('MODEL_NAME_EMB', 'text-embedding-ada-002')
+persist_directory = 'db'
 
 # Load and process the text
 embedding = OpenAIEmbeddings(model=model_name_emb)
-persist_directory = 'db'
 
 # Now we can load the persisted database from disk, and use it as normal. 
 llm = ChatOpenAI(temperature=0, model_name=model_name)
